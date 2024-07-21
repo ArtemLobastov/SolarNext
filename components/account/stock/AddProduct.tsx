67b16ad5d7b2c3faf854fbc83777a0e5f1ac7493
ArtemLobastov <artemlobastov92@gmail.com>
2024-07-21 21:17:09 +0200

    Add Product order component mockup

--- components/account/stock/AddProduct.tsx
+++ components/account/stock/AddProduct.tsx
@@ -1,9 +1,209 @@
-import { Card } from '@/components/ui/card';
+import Image from 'next/image';
+import { ChevronLeft, Upload } from 'lucide-react';
+
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Textarea } from '@/components/ui/textarea';
 
 export default function AddProduct() {
   return (
-    <Card className="h-[300px]">
-      <h1 className="text-4xl">Add product form</h1>
+    <Card className="grid flex-1 items-start gap-4 p-4 bg-muted  md:gap-8">
+      <div className="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
+        <div className="flex items-center gap-4">
+          <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
+            New Product
+          </h1>
+
+          <div className="hidden items-center gap-2 md:ml-auto md:flex">
+            <Button variant="outline" size="sm">
+              Discard
+            </Button>
+            <Button size="sm">Save Product</Button>
+          </div>
+        </div>
+        <div className="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
+          <div className="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
+            <Card x-chunk="dashboard-07-chunk-0">
+              <CardHeader>
+                <CardTitle>Product Details</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-6">
+                  <div className="grid gap-3">
+                    <Label htmlFor="name">Name</Label>
+                    <Input
+                      id="name"
+                      type="text"
+                      className="w-full"
+                      placeholder="SolarEdge SE3680H"
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="id">Id</Label>
+                    <Input
+                      id="id"
+                      type="text"
+                      className="w-full"
+                      placeholder="GGPC-001"
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="description">Description</Label>
+                    <Textarea
+                      id="description"
+                      placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc."
+                      className="min-h-32"
+                    />
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Stock</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead>Stock</TableHead>
+                      <TableHead>Price</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>
+                        <Label htmlFor="stock-1" className="sr-only">
+                          Stock
+                        </Label>
+                        <Input id="stock-1" type="number" placeholder="100" />
+                      </TableCell>
+                      <TableCell>
+                        <Label htmlFor="price-1" className="sr-only">
+                          Price
+                        </Label>
+                        <Input id="price-1" type="number" placeholder="99.99" />
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </CardContent>
+              <CardFooter className="justify-center border-t p-4"></CardFooter>
+            </Card>
+            <Card>
+              <CardHeader>
+                <CardTitle>Product Category</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-6 sm:grid-cols-3">
+                  <div className="grid gap-3">
+                    <Label htmlFor="category">Category</Label>
+                    <Select>
+                      <SelectTrigger id="category" aria-label="Select category">
+                        <SelectValue placeholder="Select category" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="frame">Frame</SelectItem>
+                        <SelectItem value="panels">Panels</SelectItem>
+                        <SelectItem value="inverters">Inverters</SelectItem>
+                        <SelectItem value="batteries">Batteries</SelectItem>
+                        <SelectItem value="backups">Backups</SelectItem>
+                        <SelectItem value="electrical">Electrical</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="subcategory">Subcategory </Label>
+                    <Select>
+                      <SelectTrigger
+                        id="subcategory"
+                        aria-label="Select subcategory"
+                      >
+                        <SelectValue placeholder="Select subcategory" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="1 phase">1 phase</SelectItem>
+                        <SelectItem value="3 phase">3 phase</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+          <div className="grid auto-rows-max items-start gap-4 lg:gap-8">
+            <Card>
+              <CardHeader>
+                <CardTitle>Product Status</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-6">
+                  <div className="grid gap-3">
+                    <Label htmlFor="status">Status</Label>
+                    <Select>
+                      <SelectTrigger id="status" aria-label="Select status">
+                        <SelectValue placeholder="Select status" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="in stock">In stock</SelectItem>
+                        <SelectItem value="low">Low stock</SelectItem>
+                        <SelectItem value="out">Out of stock</SelectItem>
+                        <SelectItem value="ordered">Ordered</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+            <Card className="overflow-hidden">
+              <CardHeader>
+                <CardTitle>Product Images</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-2">
+                  <div className="grid grid-cols-3 gap-2">
+                    <button className="flex aspect-square w-full items-center justify-center rounded-md border border-dashed">
+                      <Upload className="h-4 w-4 text-muted-foreground" />
+                      <span className="sr-only">Upload</span>
+                    </button>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+        <div className="flex items-center justify-center gap-2 md:hidden">
+          <Button variant="outline" size="sm">
+            Discard
+          </Button>
+          <Button size="sm">Save Product</Button>
+        </div>
+      </div>
     </Card>
   );
 }
--- components/account/stock/ProductOrder.tsx
+++ components/account/stock/ProductOrder.tsx
@@ -1,13 +1,190 @@
-import { Card } from '@/components/ui/card';
+import Image from 'next/image';
+import { ChevronLeft, Upload } from 'lucide-react';
+
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Textarea } from '@/components/ui/textarea';
 
 export default function ProductOrder() {
   return (
-    <Card className=" h-[300px] bg-muted">
-      <h1 className="text-2xl">Product order form</h1>
-      <Input type="text" placeholder="placeholder"></Input>
-      <Input type="text" placeholder="placeholder"></Input>
-      <Input type="text" placeholder="placeholder"></Input>
+    <Card className="grid flex-1 items-start gap-4 p-4 bg-muted  md:gap-8">
+      <div className="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
+        <div className="flex items-center gap-4">
+          <Button variant="outline" size="icon" className="h-7 w-7">
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Back</span>
+          </Button>
+          <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
+            Order SolarEdge SE3680H
+          </h1>
+          <Badge variant="outline" className="ml-auto sm:ml-0">
+            In stock
+          </Badge>
+          <div className="hidden items-center gap-2 md:ml-auto md:flex">
+            <Button variant="outline" size="sm">
+              Discard
+            </Button>
+            <Button size="sm">Order Product</Button>
+          </div>
+        </div>
+        <div className="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
+          <div className="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
+            <Card x-chunk="dashboard-07-chunk-0">
+              <CardHeader>
+                <CardTitle>Order Details</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-6">
+                  <div className="grid gap-3">
+                    <Label htmlFor="name">Name</Label>
+                    <Input
+                      id="name"
+                      type="text"
+                      className="w-full"
+                      defaultValue="SolarEdge SE3680H"
+                      disabled
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="id">Id</Label>
+                    <Input
+                      id="id"
+                      type="text"
+                      className="w-full"
+                      defaultValue="GGPC-001"
+                      disabled
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="description">Description</Label>
+                    <Textarea
+                      id="description"
+                      defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc."
+                      className="min-h-32"
+                    />
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+            <Card x-chunk="dashboard-07-chunk-1">
+              <CardHeader>
+                <CardTitle>Order Amount</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="w-[100px]">id</TableHead>
+                      <TableHead>Amount</TableHead>
+                      <TableHead>Price</TableHead>
+                      <TableHead>Total</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell className="font-semibold">GGPC-001</TableCell>
+                      <TableCell>
+                        <Label htmlFor="stock-1" className="sr-only">
+                          Stock
+                        </Label>
+                        <Input id="stock-1" type="number" defaultValue="100" />
+                      </TableCell>
+                      <TableCell>
+                        <Label htmlFor="price-1" className="sr-only">
+                          Price
+                        </Label>
+                        <Input
+                          id="price-1"
+                          type="number"
+                          defaultValue="99.99"
+                          disabled
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <Label htmlFor="total-1" className="sr-only">
+                          Total
+                        </Label>
+                        <Input
+                          id="total-1"
+                          type="number"
+                          defaultValue="999.99"
+                          disabled
+                        />
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </CardContent>
+              <CardFooter className="justify-center border-t p-4"></CardFooter>
+            </Card>
+          </div>
+          <div className="grid auto-rows-max items-start gap-4 lg:gap-8">
+            <Card className="overflow-hidden" x-chunk="dashboard-07-chunk-4">
+              <CardHeader>
+                <CardTitle>Product Images</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-2">
+                  <Image
+                    alt="Product image"
+                    className="aspect-square w-full rounded-md object-cover"
+                    height="300"
+                    src="/images/SolarEdge-SE3680H-1.jpg"
+                    width="300"
+                  />
+                  <div className="grid grid-cols-3 gap-2">
+                    <button>
+                      <Image
+                        alt="Product image"
+                        className="aspect-square w-full rounded-md object-cover"
+                        height="84"
+                        src="/images/SolarEdge-SE3680H-2.png"
+                        width="84"
+                      />
+                    </button>
+
+                    <button className="flex aspect-square w-full items-center justify-center rounded-md border border-dashed">
+                      <Upload className="h-4 w-4 text-muted-foreground" />
+                      <span className="sr-only">Upload</span>
+                    </button>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+        <div className="flex items-center justify-center gap-2 md:hidden">
+          <Button variant="outline" size="sm">
+            Discard
+          </Button>
+          <Button size="sm">Save Product</Button>
+        </div>
+      </div>
     </Card>
   );
 }
